2beee6497d1bc0635ccc6cc0e15fef0533312c99
Liz A <lizethariasc14@gmail.com>
2022-06-16 17:14:55 -0500

    created blocks

--- app.js
+++ app.js
@@ -12,13 +12,35 @@ class Block {
     }
 }
 
+//All my blocks
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+    
+]
+
 //draw block 
-function addBlock() {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    block.style.left = '100px'
-    block.style.bottom = '50px'
-    grid.appendChild(block);
+function addBlocks() {
+   for(let i = 0; i < blocks.length; i++) {
+       const block = document.createElement('div');
+       block.classList.add('block');
+       block.style.left = blocks[i].bottomLeft[0] + 'px'
+       block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+       grid.appendChild(block);
+    }
 }
 
-addBlock()
+addBlocks()
